de21fef29eeb7e1be3b68ff60a05f153eb65d0f8
Cha Yang <chachaiyang3@gmail.com>
2019-09-14 22:34:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-  def argument
-end
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 0
+  while i < array.length
+    puts "#{i + 1}. #{array[i]}"
+    i += 1
+  end
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+roll_call_dwarves(dwarves)
