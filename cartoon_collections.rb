12b826f9e902cf895fd71fb4b6e040c4cd6f3607
Cha Yang <chachaiyang3@gmail.com>
2019-09-14 22:57:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,5 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls  = ["earth", "wind", "fire", "water", "heart"]
 
 def roll_call_dwarves(array)
   i = 0
@@ -8,3 +9,31 @@ def roll_call_dwarves(array)
   end
 end
 roll_call_dwarves(dwarves)
+
+def summon_captain_planet(array)
+  new_array = []
+  i = 0
+  while i < array.length
+    new_array << array[i].capitalize + "!"
+    i += 1
+  end
+    p new_array
+end
+summon_captain_planet(planeteer_calls)
+
+def long_planeteer_calls(array)
+  i  = 0
+  if array.any? {[i], i.length > 4}
+    return true
+  else
+    return false
+    i += 1
+  end
+end
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |type|
+    cheese_types.include?(type)
+  end
+end
